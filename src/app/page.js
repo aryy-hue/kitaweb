5888c1301d1bbc99ea41f6d0370498056a2c0a80
TedyPermana24 <tedysukma24@gmail.com>
2023-10-04 21:29:21 +0700

    section 2 responsive added

--- src/app/page.js
+++ src/app/page.js
@@ -41,6 +41,82 @@ export default function page() {
           <p class="md:text-lg text-base text-slate-500">You can have a ready-made website for only a small fee. Complete features will facilitate the website creation process, from Auto-Install WordPress, Mockup, etc. 99.99% server uptime guarantee allows your website to be online all the time.</p>
         </div>
       </section>
+
+      <section class="bg-[#FFFADD] md:grid grid-cols-2 grid-rows-2 items-center md:grid-rows-1 gap-10 mt-20 px-10 py-20"> 
+        <div class="pt-10 md:mt-0">
+          <h3 class="md:text-2xl text-xl font-bold text-slate-600">Special Customer Service</h3>
+          <h2 class="md:text-3xl text-3xl font-bold pt-2 md:my-6">Number 1 Technical Support In Indonesia</h2>
+          <p class="md:text-lg text-base text-slate-500 pt-2 text-justify">Contact KitaWeb’s Customer Success Agent at any time when there is a problem with your website. No need to wait long, your problem will be resolved quickly and in real-time via Whatsapp or email.</p>
+        </div>
+        <div class="py-10">
+        <Image
+                src="/rocket.svg"
+                alt="Rocket"
+                width={600}
+                height={600}
+          />
+        </div>
+      </section>
+
+      <section class="bg-[#3156D7] items-center grid-cols-2 grid-rows-2 py-20 gap-3 px-10 md:grid-rows-1 md:grid">
+      <div class="py-10">
+      <Image  
+                src="/planet.svg"
+                alt="planet"
+                width={600}
+                height={600}
+          />
+      </div>
+        <div class="ml-5 md:mt-0">
+          <h2 class="md:text-5xl text-4xl font-bold text-white my-4 md:my-6">About us</h2>
+          <p class="md:text-lg text-base text-slate-500 text-white text-justify">You can have a ready-made website for only a small fee. Complete features will facilitate the website creation process, from Auto-Install WordPress, Mockup, etc. 99.99% server uptime guarantee allows your website to be online all the time.</p>
+        </div>
+      </section>
+
+      <section class="bg-[#22273C] h-2/5 grid md:grid-cols-2 grid-rows-2 md:grid-rows-1 px-14 md:py-10 md:px-10 text-white">
+        <div class="pt-10 md:pt-0">
+          <Image
+                    src="/logo-kitaweb.svg"
+                    alt="kitaweb logo"
+                    width={100}
+                    height={24}
+              />
+          <p class="pt-5">Our mission is to help millions of people harness the potential</p>
+          <p>of the internet to build success online.</p>
+        </div>
+        <div class="pt-5 md:pl-28 xl:pl-72">
+          <p>For more information :</p>
+          <div class="items-center mt-3">
+          <Link href="" class="rounded-full overflow-hidden mr-3">
+            <div class="bg-[#393F53] p-4 rounded-full w-fit inline-block">
+              <Image 
+                src="/footer-socmed/facebook.svg"
+                width={30}
+                height={30}
+              />
+            </div>
+          </Link>
+          <Link href="" class="rounded-full overflow-hidden mr-3">
+            <div class="bg-[#393F53] p-4 rounded-full w-fit inline-block">
+              <Image 
+                src="/footer-socmed/twitter.svg"
+                width={30}
+                height={30}
+              />
+            </div>
+          </Link>
+          <Link href="" class="rounded-full overflow-hidden mr-3">
+            <div class="bg-[#393F53] p-4 rounded-full w-fit inline-block">
+              <Image 
+                src="/footer-socmed/instagram.svg"
+                width={30}
+                height={30}
+              />
+            </div>
+          </Link>
+          </div> 
+        </div>
+      </section>
     </> 
   )
 }
