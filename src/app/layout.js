35954df4dd8f7c3e7221b2df29ec3f28db351e7b
TedyPermana24 <tedysukma24@gmail.com>
2023-10-07 16:46:47 +0700

    add firebase backend (modal still bugged)

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,10 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+ 
+const inter = Inter({
+  weight: '400',
+  subsets: ['latin'],
+})
 
 export const metadata = {
   title: 'Kitaweb',
